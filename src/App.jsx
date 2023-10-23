8cdae303b972e3b52e5534468a71e9b7a432c395
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-10-23 11:33:59 +0200

    boiler

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Signup from "./views/signup/Signup";
 import Profile from "./views/profile/Profile";
 import ListMeetup from "./views/listMeetup/ListMeetup";
 import MeetupInfo from "./views/meetupInfo/MeetupInfo";
+import AppLayout from "./views/appLayout/AppLayout";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="meetup-apps">
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="listMeetup" element={<ListMeetup />} />
-          <Route path="meetupInfo/:id" element={<MeetupInfo />} />
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="listMeetup" element={<ListMeetup />} />
+            <Route path="meetupInfo/:id" element={<MeetupInfo />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/views/appLayout/AppLayout.jsx
@@ -0,0 +1,5 @@
+import "./AppLayout.css";
+
+export default function AppLayout() {
+  return <div className="main-container"></div>;
+}
